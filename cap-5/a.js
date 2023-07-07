649c95f53daa8e983e476e49377a0c10a27102f3
trintaetrez <fernandocdacruz@gmail.com>
2023-07-07 18:55:17 -0300

    15

--- cap-5/a.js
+++ cap-5/a.js
@@ -2,7 +2,9 @@ const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
 frm.addEventListener("submit", (e) => {
+    
     e.preventDefault()
+
     const fruta = frm.inFruta.value
     const num = Number(frm.inNumero.value)
 
