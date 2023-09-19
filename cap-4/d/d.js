06143b902a239f30517b9a2d6ee350dcdbb6b5bf
trintaetrez <fernandocdacruz@gmail.com>
2023-09-18 22:50:23 -0300

    31

--- cap-4/d/d.js
+++ cap-4/d/d.js
@@ -1,1 +1,25 @@
-const frm = document.querySelector
+const frm = document.querySelector("form");
+const resp1 =  document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const a = Number(frm.inA.value);
+    const b = Number(frm.inB.value);
+    const c = Number(frm.inC.value);
+
+    if ( a + b > c && b + c > a && c + a > b ) {
+        resp1.innerText = `Lados podem formar um triângulo.`
+
+        if ( a == b && b == c) {
+            resp2.innerText = `Tipo: Equilátero`
+        } else if (a == b && b != c) {
+            resp2.innerText = `Tipo: Isóceles`
+        } if ( a != b && b != c) {
+            resp2.innerText = `Tipo: Escaleno`
+        }
+    } else {
+        resp1.innerText = `Lados não podem formar um triângulo.`
+    }
+})
