ad49face20a12837ceee6a4d2f3cabee4cd3179f
trintaetrez <fernandocdacruz@gmail.com>
2023-09-20 18:15:12 -0300

    33

---
+++ cap-5/ex5-3/1.js
@@ -0,0 +1,13 @@
+let num // declara variável num com let, pois ela pode ser alterada
+        // e será acessada fora do bloco do.. while
+do {    // cria laço de repetição (faça...)
+    num = Number(prompt("Número: ")) // lê um número
+    if (num == 0  || isNaN(num)) {   // se num=0 ou é inválido
+        alert("Digite um número válido...")
+    }
+} while (num == 0 || isNaN(num))     // ... enquanto num=0 ou inválido
+let pares = `Pares entre 1 e ${num}: ` // string que irá conter a resposta
+for (let i = 2; i <= num; i = i + 2) {
+    pares = pares + i + ", "
+}
+alert(pares) // exibe lista dos números pares
