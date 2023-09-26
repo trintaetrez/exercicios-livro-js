fa83977bc0387386fd9a6e3e0979a44cebdb56a3
trintaetrez <fernandocdacruz@gmail.com>
2023-09-25 23:26:30 -0300

    36

--- cap-6/ex6-1/1.js
+++ cap-6/ex6-1/1.js
@@ -5,13 +5,13 @@ const respLista = document.querySelector("pre")
 const pacientes = [] // declara vetor global
 
 frm.addEventListener("submit", (e) => {
-    
     e.preventDefault() // evita envio do form
     const nome = frm.inPaciente.value // obtém nome do paciente
     pacientes.push(nome) // adiciona o nome no final do vetor
-    let lista ="" // string para concatenar pacientes
+    let lista = "" // string para concatenar pacientes
 
     // for "tradicional" (inicia em 0, enquanto menor que tamanho do array)
+    
     for (let i = 0; i < pacientes.length; i++) {
         lista += `${i + 1}. ${pacientes[i]}\n`
     }
@@ -19,19 +19,17 @@ frm.addEventListener("submit", (e) => {
     respLista.innerText = lista // exibe a lista de pacientes na página
     frm.inPaciente.value = "" // limpa conteúdo do campo de formulário
     frm.inPaciente.focus() // posiciona o cursor no campo
-
 })
 
-// adiciona um "ouvinte" para o evento click no btUrgencia que está no form
-frm.btUrgencia.addEventListener("click", () => {
+// adicionar um "ouvinte" para o evento click no btUrgencia que esta no form
 
+frm.btUrgencia.addEventListener("click", () => {
     // verifica se as validações do form estão ok (no caso, paciente is required)
     if (!frm.checkValidity()) {
         alert("Informe o nome do paciente a ser atendido em caráter de urgência")
         frm.inPaciente.focus() // posiciona o cursor no campo inPaciente
         return // retorna ao form
     }
-
     const nome = frm.inPaciente.value // obtém nome do paciente
     pacientes.unshift(nome) // adiciona paciente no início do vetor
     let lista = "" // string para concatenar pacientes
@@ -41,20 +39,4 @@ frm.btUrgencia.addEventListener("click", () => {
     respLista.innerText = lista // exibe a lista de pacientes na página
     frm.inPaciente.value = "" // limpa conteúdo do campo de formulário
     frm.inPaciente.focus() // posiciona o cursor no campo
-})
-
-frm.btAtender.addEventListener("click", () => {
-
-    // se o tamanho do vetor = 0
-    if (pacientes.length == 0) {
-        alert("Não há pacientes na lista de espera")
-        frm.inPaciente.focus()
-        return
-    }
-
-    const atender = pacientes.shift() // remove do inicio da fila (e obtém nome)
-    respNome.innerText = atender // exibe o nome do paciente em atendimento
-    let lista = "" // string para concatenar pacientes
-    pacientes.forEach((paciente,i) => (lista += `${i + 1}. ${paciente}\n`))
-    respLista.innerText = lista // exibe a lista de pacientes na página
 })
