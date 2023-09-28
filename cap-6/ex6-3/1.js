fff18e9c403fff6c0ecf253c0e7b03b42ce9db75
trintaetrez <fernandocdacruz@gmail.com>
2023-09-28 19:30:48 -0300

    39

--- cap-6/ex6-3/1.js
+++ cap-6/ex6-3/1.js
@@ -15,4 +15,49 @@ frm.addEventListener("submit", (e) => {  // "escuta" evento submit do form
     frm.btListar.dispatchEvent(new Event("click")) 
 })
 
-console.log(carros);
+frm.btListar.addEventListener("click", () => {  // "escuta clique em btListar"
+    if (carros.length == 0) { // se o tamanho do vetor é igual a 0
+        alert("Não há carros na lista")
+        return
+    }
+    // método reduce() concatena uma string, obtendo modelo e preço de cada veículo
+    const lista = carros.reduce((acumulador, carro) =>
+    acumulador + carro.modelo + " - R$: " + carro.preco.toFixed(2) + "\n", "")
+    resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}`
+})
+
+frm.btFiltrar.addEventListener("click", () => {
+const maximo = Number(prompt("Qual o valor máximo que o cliente deseja pagar?"))
+
+if (maximo == 0 || isNaN(maximo)) {  // se não informou ou valor inválido
+    return  // ... retorna
+}
+// cria um novo vetor com os objetos que atendem a condição de filtro
+const carrosFilter = carros.filter(carro => carro.preco <= maximo)
+if (carrosFilter.length == 0) { // se tamanho do vetor filtrado é 0
+    alert("Não há carros com preço inferior ou igual ao solicitado")
+    return
+}
+let lista = ""
+for (const carro of carrosFilter) {  // percorre cada elemento do array
+    lista += `${carro.modelo} - R$ ${carro.preco.toFixed(2)}\n`
+}
+resp.innerText = `Carros Até R$: ${maximo.toFixed(2)}\n${"-".repeat(40)}\n${lista}`
+}) 
+
+frm.btSimular.addEventListener("click", () => {
+    const desconto = Number(prompt("Qual o percentual de desconto: "))
+    if (desconto == 0 || isNaN(desconto)) { // se não informou ou valor inválido
+        return  // ... retorna
+    }
+    const carrosDesc = carros.map(aux => ({
+        modelo: aux.modelo,
+        preco: aux.preco - (aux.preco * desconto / 100)
+    }))
+    let lista = ""
+    for (const carro of carrosDesc) { // percorre cada elemento do array
+        lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`
+    }
+    resp.innerText = `Carros com desconto: ${desconto}%\n${"-".repeat(40)}\n${lista}`
+})
+
