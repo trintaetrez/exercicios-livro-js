e0f45451715b290bd91c79b2ce8c45113ab5c40b
trintaetrez <fernandocdacruz@gmail.com>
2023-10-03 22:38:05 -0300

    41

--- cap-6/ex6-4/1.js
+++ cap-6/ex6-4/1.js
@@ -21,7 +21,7 @@ frm.btListar.addEventListener("click", () => {
     let lista = "" // para concatenar lista de crianças
     for (const crianca of criancas) {
         const { nome, idade } = crianca // desestrutura o objeto
-        lista += nome + " - " + idade + "anos\n"
+        lista += nome + " - " + idade + " anos\n"
     }
     resp.innerText = lista // exibe a lista
 })
@@ -32,4 +32,27 @@ frm.btResumir.addEventListener("click", () => {
         return
     }
     const copia = [...criancas] // cri uma cópia do vetor criança
+    copia.sort((a, b) => a.idade - b.idade) // ordena pela idade
+    let resumo = ""
+    let aux = copia[0].idade // menor idade do vetor ordenado
+    let nomes = [] // para inserir nomes de cada idade
+    for (const crianca of copia) {
+        const { nome, idade } = crianca
+        if (idade == aux) { // se é da mesma idade auxiliar...
+            nomes.push(nome) // adiciona ao vetor nomes
+        }
+        else { // senão, monta o resumo para cada idade
+        resumo += aux + " ano(s): " + nomes.length + " crianças(s) - "
+        resumo += ((nomes.length / copia.length) * 100).toFixed(2) + "%\n"
+        resumo += "(" + nomes.join(", ") + ")\n\n"
+        aux = idade // obtém a nova idade na ordem
+        nomes = [] // limpa o vetor dos nomes
+        nomes.push(nome) // adiciona o primeiro da nova idade 
+        } 
+    }
+    // adiciona a última criança
+    resumo += aux + " ano(s): " + nomes.length + " crianças(s) - "
+    resumo += ((nomes.length / copia.length) * 100).toFixed(2) + "%\n"
+    resumo += "(" + nomes.join(", ") + ")\n\n"
+    resp.innerText = resumo // exibe a resposta
 })
