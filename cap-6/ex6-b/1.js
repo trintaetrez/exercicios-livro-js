35beeb3358e27157f9c1ad8730f82bc8ab4d6fee
trintaetrez <fernandocdacruz@gmail.com>
2023-10-05 13:29:28 -0300

    44

--- cap-6/ex6-b/1.js
+++ cap-6/ex6-b/1.js
@@ -3,3 +3,39 @@ const resp1 = document.querySelector("#outLista")
 const resp2 = document.querySelector("#outOrdem")
 
 const numeros = []
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const num = Number(frm.inNumero.value)
+    if (numeros.includes(num)) {
+        alert("Esse número já foi digitado !!")
+        frm.inNumero.focus()
+        return
+    } else {
+        numeros.push(num)
+        resp1.innerText = `Números: ${numeros.join(", ")}`
+        frm.inNumero.value = ""
+        frm.inNumero.focus()
+    }
+})
+
+frm.btVerificar.addEventListener("click", () => {
+    if (numeros.length == 0) {
+        alert("Não há números na lista...")
+        inNum.focus()
+        return
+      }
+    /*const copia = [...numeros]
+    console.log(copia);
+    const copiaOrdenada = copia.sort((a, b) => a - b)
+    console.log(copiaOrdenada);*/
+    const array1 = `${numeros.join(", ")}`
+    const copia = [...numeros]
+    const copiaOrdenada = copia.sort((a, b) => a - b)
+    const array2 = `${copiaOrdenada.join(", ")}`
+    if (array1 === array2) {
+        resp2.innerText = `Números estão em ordem crescente !`
+    } else {
+        resp2.innerText = `Atenção... Números não estão em ordem crescente !`
+    }
+})
