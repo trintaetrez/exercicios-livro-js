941c441e706b4b51c99261bc893708ce929c4a8f
trintaetrez <fernandocdacruz@gmail.com>
2023-10-10 13:13:04 -0300

    48

--- cap-7/ex7-6/1.js
+++ cap-7/ex7-6/1.js
@@ -13,6 +13,28 @@ frm.addEventListener("submit", (e) => {
 
     // verifica se não possui números
     if (senha.match(/[0-9]/g) == null) {
-        
+        erros.push("possuir números(no mínimo, 1)")
+    }
+
+    // verifica se não possui letras minúsculas
+    if (!senha.match(/[a-z]/g)) {
+        erros.push("possuir letras minúsculas (no mínimo,1)")
+    }
+
+    // verifica se não possui letras maiúsculas ou se possui apenas 1
+    if (!senha.match(/[A-Z]/g) || senha.match(/[A-Z]/g).length == 1) {
+        erros.push("possuir letras maiúsculas (no mínimo, 2)")
+    }
+
+    // verifica se não possui símbolos ou "_"
+    if (!senha.match(/[\W|_]/g)) {
+        erros.push("possuir símbolos (no mínimo, 1)")
+    }
+
+    // se o vetor está vazio (significa que não foram encontrados erros)
+    if (erros.length == 0) {
+        resp.innerText = "Ok! Senha válida"
+    } else {
+        resp.innerText = `Erro... A senha deve ${erros.join(", ")}`
     }
 })
