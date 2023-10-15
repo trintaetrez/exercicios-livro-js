23277e17c6888b955e60511311ff9c235fff7468
trintaetrez <fernandocdacruz@gmail.com>
2023-10-15 09:40:38 -0300

    51

--- cap-7/ex7-10/a/1.js
+++ cap-7/ex7-10/a/1.js
@@ -1,1 +1,37 @@
-// ler uma mensagem
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const mensagem = frm.inMensagem.value
+    let resposta = ""
+    const tam = mensagem.length
+
+    for (let i = 1; i < tam; i = i + 2) {
+        resposta += mensagem.charAt(i)
+    }
+
+    for (let i = 0; i < tam; i = i + 2) {
+        resposta += mensagem.charAt(i)
+    }
+
+    resp.innerText = resposta
+})
+
+frm.btDecript.addEventListener("click", () => {
+    // verifica se as validações do form estão ok (no caso, mensagem is required)
+    if (!frm.checkValidity()) {
+      alert("Informe a mensagem criptografada")
+      frm.inMensagem.focus() // posiciona o cursor no campo
+      return // retorna ao form
+    }
+    const mensagem = frm.inMensagem.value          // conteúdo do campo
+  
+    resposta = mensagem
+  
+    resp.innerText = resposta
+  })
+
+  frm.btReset.addEventListener("click", () => {
+    resp.innerText = ""
+  })
