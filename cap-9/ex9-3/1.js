286bb707ababe32135fd7454785d5840f18bc040
trintaetrez <fernandocdacruz@gmail.com>
2023-11-01 13:25:31 -0300

    65

--- cap-9/ex9-3/1.js
+++ cap-9/ex9-3/1.js
@@ -31,9 +31,94 @@ frm.reset()          // limpa o form
 frm.inNome.focus()   // joga o foco (cursor) no campo inNome
 })
 
-const varApostaExiste = (peso) => {
-    if (localStorage.getItem(melanciaPeso)) { // se existir dados em localStorage
+const verApostaExiste = (peso) => {
+    if (localStorage.getItem("melanciaPeso")) { // se existir dados em localStorage
         // obtém seu conteúdo e a string é dividida em itens de vetor a cada ";"
         const pesos = localStorage.getItem("melanciaPeso").split(";")
+
+        // o peso deve ser convertido em string, pois o vetor contém strings
+        return pesos.includes(peso.toString())
+} else {
+    return false
 }
 }
+
+const mostrarApostas = () => {
+    // se não há apostas armazenadas em localStorage
+    if (!localStorage.getItem("melanciaNome")) {
+        // limpa o espaço de exibição das apostas (para quando "Limpar Apostas")
+        respLista.innerText = ""
+        return // retorna (não executa os comandos abaixo)
+    }
+
+    // obtém o conteúdo das variáveis salvas no localStorage, separando-as
+    // em elementos de vetor a cada ocorrência do ";"
+    const nomes = localStorage.getItem("melanciaNome").split(";")
+    const pesos = localStorage.getItem("melanciaPeso").split(";")
+
+    let linhas = "" // irá acumular as linhas a serem exibidas
+
+    // repetição para percorrer todos os elementos do vetor
+    for (let i = 0; i < nomes.length; i++) {
+        // concatena em linhas os nomes dos apostadores e suas apostas
+        linhas += nomes[i] + " - " + pesos[i] + "gr \n"
+    }
+
+    // exibe as linhas (altera o conteúdo do elemento respLista)
+    respLista.innerText = linhas
+}
+// chama a function quando a página é carregada, para mostrar apostas salvas
+window.addEventListener("load", mostrarApostas)
+
+frm.btVencedor.addEventListener("click", () => {
+    // se não há apostas armazenadas em localStorage
+    if (!localStorage.getItem("melanciaNome")) {
+        alert("Não há apostas cadastradas")
+        return // retorna (não executa os comandos abaixo)
+    }
+
+    // solicita o pesso correto da melancia
+    const pesoCorreto = Number(prompt("Qual o peso correto da melancia?"))
+
+    // se não informou, retorna
+    if (pesoCorreto == 0 || isNaN(pesoCorreto)) {
+        return
+    }
+
+    // obtém os dados armazenados, separando-os em elementos de vetor
+    const nomes = localStorage.getItem("melanciaNome").split(";")
+    const pesos = localStorage.getItem("melanciaPeso").split(";")
+
+    // valor inicial para vencedor é o da primeira aposta
+    let vencedorNome = nomes[0]
+    let vencedorPeso = Number(pesos[0])
+
+    // percorrer as apostas
+    for (let i = 1; i < nomes.lenght; i++) {
+        // calcula a diferença de peso do "vencedor" e da aposta atual
+        const difVencedor = Math.abs(vencedorPeso - pesoCorreto)
+        const difAposta = Math.abs(Number(pesos[i]) - pesoCorreto)
+
+        // se a diferença de aposta atual (no for) for menor que a do "vencedor"
+        if (difAposta < difVencedor) {
+            vencedorNome = nomes[i]         // troca o "vencedor"
+            vencedorPeso = Number(pesos[i]) // para este elemento
+        }
+    }
+
+    // monta mensagem com dados do vencedor
+    let mensagem = "Resultado - Peso Correto: " + pesoCorreto + "gr"
+    mensagem += "\n----------------------------------------------"
+    mensagem += "\nVencedor: " + vencedorNome
+    mensagem += "\nAposta: " + vencedorPeso + "gr"
+    alert(mensagem)
+})
+
+frm.btLimpar.addEventListener("click", () => {
+    // solicita confirmação para excluir as apostas
+    if (confirm("Confirma exclusão de todas as apostas?")) {
+        localStorage.removeItem("melanciaNome") // remove as variáveis salvas
+        localStorage.removeItem("melanciaPeso") // em localStorage
+        mostrarApostas()                        // exibe a listagem vazia
+    }
+})
